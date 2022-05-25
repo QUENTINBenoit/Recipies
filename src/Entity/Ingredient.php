86d3e9d5782b6e0c3d5a8b1a64d8bb999b09902c
github_QUENTINBenoit <benquelm@gmail.com>
2022-05-25 22:00:03 +0200

    Feature : add Entity Igredient and validation

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\length(min: 2, max: 50)]
+    private $name;
+
+    #[ORM\Column(type: 'float')]
+    #[Assert\NotNull]
+    #[Assert\Positive]
+    private $price;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Assert\NotNull]
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
