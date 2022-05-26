590d02d64dbf2b3c4af2ae9e850ca8dbbba25781
github_QUENTINBenoit <benquelm@gmail.com>
2022-05-26 17:15:29 +0200

    ✨ feature : display all ingrdients on twigIngredient and paginator

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IngredientController extends AbstractController
+{
+
+    /**
+     * This function display all ingredients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'ingredient', methods: ['GET'])]
+    public function ingredient(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+        return $this->render('ingredient/index.html.twig', [
+            'ingredients' => $ingredients
+        ]);
+    }
+}
