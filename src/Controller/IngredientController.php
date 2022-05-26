c107f8f00884bf71d6388983f75cc0fac6219382
github_QUENTINBenoit <benquelm@gmail.com>
2022-05-26 19:06:59 +0200

    🌟 feature :  Creation d'un formulaire d'ajout d'ingrédients

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/ingredient', name: 'ingredient_')]
 class IngredientController extends AbstractController
 {
 
@@ -20,8 +25,8 @@ class IngredientController extends AbstractController
      * @param Request $request
      * @return Response
      */
-    #[Route('/ingredient', name: 'ingredient', methods: ['GET'])]
-    public function ingredient(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    #[Route('/', name: 'list', methods: ['GET'])]
+    public function listIngredient(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
 
         $ingredients = $paginator->paginate(
@@ -29,8 +34,31 @@ class IngredientController extends AbstractController
             $request->query->getInt('page', 1),
             10
         );
-        return $this->render('ingredient/index.html.twig', [
+        return $this->render('ingredient/list.html.twig', [
             'ingredients' => $ingredients
+        ]);
+    }
+
+
+    #[Route('/add', name: 'ingredient_add', methods: ['GET', 'POST'])]
+    public function addIngredient(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //\dd($form->getData()); récupération des infos de mon formulaire
+            $em = $doctrine->getManager();
+            $em->persist($ingredient);
+            $em->flush();
+            // petit  message flash pour indiquer la réussite de la création d'un nouvel ingrédient
+            $this->addFlash('badge rounded-pill bg-info mt-2 text-white', 'l\'ingrédient ' . $ingredient->getName() . 'a bien été ajouté.');
+            return $this->redirectToRoute('ingredient_list');
+        }
+        // sinon j'affiche un formulaire d'ajout d'un ingredient
+        return $this->render('ingredient/add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -6,9 +6,11 @@ use App\Repository\IngredientRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use ProxyManager\ProxyGenerator\PropertyGenerator\PublicPropertiesMap;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
+#[UniqueEntity('name')]
 class Ingredient
 {
     #[ORM\Id]
