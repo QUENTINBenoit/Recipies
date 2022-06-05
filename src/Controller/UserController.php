edadf66beb2d05d2a68f3dbc583a7fa5467bc87b
github_QUENTINBenoit <benquelm@gmail.com>
2022-06-05 23:02:09 +0200

    ✨ mise en place CRUD User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,26 +3,92 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/user', name: 'user_')]
+#[Route('/user', name: 'user_', requirements: ['id' => '\d+'])]
 class UserController extends AbstractController
 {
     /**
-     * Methode permattant d'afficher la liste de Users
+     * Methode permettant d'afficher la liste de Users
      *
      * @param UserRepository $userRepository
      * @return Response
      */
-    #[Route('/list', name: 'list')]
+    #[Route('/list', name: 'list', methods: ['GET'])]
     public function listUser(UserRepository $userRepository): Response
     {
-        dump($userRepository->findAll());
         return $this->render('user/list.html.twig', [
             'listUsers' => $userRepository->findAll(),
         ]);
+    }
+    /**
+     * Méthode permettant de mettre a jour  d'un utilisateur 
+     *
+     * @param Request $request
+     * @param User $user
+     * @param EntityManagerInterface $doctrine
+     * @param UserPasswordHasherInterface $userPasswordHasherInterface
+     * @return Response
+     */
+    #[Route('/edit/{id}', name: 'edit', methods: ['GET', 'POST'])]
+    public function newUser(
+        Request $request,
+        User $user,
+        EntityManagerInterface $doctrine,
+        UserPasswordHasherInterface $userPasswordHasherInterface
+    ): Response {
+        // \dd('formulaire d\'ajout');
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Je récuère la mot de passe en clair
+            $passowrd = $form->get('password')->getData();
+            // Je Hash le mot de passe
+            $hashPassword = $userPasswordHasherInterface->hashPassword($user, $passowrd);
+            $user->setPassword($hashPassword);
+            // sauvegarde en BDD
+            $doctrine->persist($user);
+            $doctrine->flush();
+            $this->addFlash(
+                'badge rounded-pill bg-info mt-2 text-white text-center',
+                'Les informations de l\'utilitsateur ' . $user->getFullName() . ' a bien été mise à jour'
+            );
+            return $this->redirectToRoute('user_list', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->renderForm('user/edit.html.twig', [
+            'user' => $user,
+            'formUser' => $form,
+        ]);
+    }
+    /**
+     * Méthode permettant de supprimer un ultilisateur 
+     *
+     * @param User $user
+     * @param Request $request
+     * @param EntityManagerInterface $doctrine
+     * @return Response
+     */
+    #[Route('/{id}', name: 'delete',  methods: ['GET', 'POST'])]
+    public function deleteUser(
+        User $user,
+        Request $request,
+        EntityManagerInterface $doctrine
+    ): Response {
+
+        $doctrine->remove($user);
+        $doctrine->flush();
+        $this->addFlash(
+            'badge rounded-pill bg-info mt-2 text-white text-center',
+            'L\'utilitsateur ' . $user->getFullName() . ' a bien été suprimé'
+        );
+
+        return $this->redirectToRoute('user_list', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -37,6 +39,38 @@ class UserType extends AbstractType
                     'class' => 'form-label mt-4'
                 ],
             ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Adresse email',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                    'label' => 'Mot de passe',
+                    'label_attr' => [
+                        'class' => 'form-label mt-5'
+                    ]
+                ],
+                'second_options' => [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                    'label' => 'Confirmation du mot de passe',
+                    'label_attr' => [
+                        'class' => 'form-label'
+                    ]
+                ],
+                'invalid_message' => 'les mots de passe ne correspondent pas.'
+            ])
+
             ->add('submit', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-primary mt-4'
