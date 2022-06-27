432e3cefb8a6995e61b378a5c0c0fbe5b59a8534
github_QUENTINBenoit <benquelm@gmail.com>
2022-06-27 16:23:00 +0200

    recuperation des infos dans le formulaire de recherche

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +19,17 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home', methods: 'GET')]
     public function home(RecipeRepository $recipeRepository): Response
     {
+        return $this->render('home/home.html.twig', [
+            'recipiesPublic' => $recipeRepository->findPubicRecipes(3),
+        ]);
+    }
+    #[Route('/search', name: 'search', methods: 'GET')]
+    public function search(RecipeRepository $recipeRepository, Request $request): Response
+    {
+        $searchValue =  $request->get('q');
+        \dd($searchValue);
+        $recipeSearch = $recipeRepository->findSearchByTitle($searchValue);
+        \dd($recipeSearch);
         return $this->render('home/home.html.twig', [
             'recipiesPublic' => $recipeRepository->findPubicRecipes(3),
         ]);
